8edcf6bea1d5505690d44df7a115eba8c634ce46
Ewan Evans <ewan.evans@dhsc.gov.uk>
2024-03-27 11:31:52 +0000

    calculator A working, calculator B not

--- stock_and_flow.R
+++ stock_and_flow.R
@@ -14,83 +14,79 @@ ui <- fluidPage(
         tabsetPanel(
             tabPanel('Calculator',
                      h2("Inputs:"),
-                     numericInput(inputId = 'pre_DRD_los', 
+                     numericInput(inputId = 'pre_DRD_los_in', 
                                   label = 'Select average pre-DRD length of stay:', 
                                   value = NA, 
                                   min = 0, 
                                   max = 20, 
                                   step = 0.5),
-                     numericInput(inputId = 'post_DRD_los', 
+                     numericInput(inputId = 'post_DRD_los_in', 
                                   label = 'Select average post-DRD length of delay (all patients):', 
                                   value = NA, 
                                   min = 0, 
                                   max = 20, 
                                   step = 0.5),
-                     numericInput(inputId = 'bed_occ', 
+                     numericInput(inputId = 'bed_occ_in', 
                                   label = 'Select bed occupancy (%):', 
                                   value = NA, 
                                   min = 50, 
                                   max = 100, 
                                   step = 1),
-                     numericInput(inputId = 'ga_beds', 
+                     numericInput(inputId = 'ga_beds_in', 
                                   label = 'Select G&A beds:', 
                                   value = NA, 
                                   min = 50, 
                                   max = 2000, 
                                   step = 10),
+                     numericInput(inputId = 'nctr_per_bed_in', 
+                                  label = 'Select NCTR per bed:', 
+                                  value = NA, 
+                                  min = 0, 
+                                  max = 1, 
+                                  step = 0.01),
+                     numericInput(inputId = 'total_admissions_in', 
+                                  label = 'Select total admissions:', 
+                                  value = NA, 
+                                  min = 0, 
+                                  max = 1000, 
+                                  step = 5),
                      h2("Ouputs:"),
-                     strong(textOutput(outputId = 'patient_stock')),
-                     strong(textOutput(outputId = 'ctr_stock')),
-                     strong(textOutput(outputId = 'nctr_stock')),
-                     strong(textOutput(outputId = 'total_admissions')),
-                     strong(textOutput(outputId = 'avg_los')),
-                     strong(textOutput(outputId = 'nctr_per_bed'))
+                     strong(textOutput(outputId = 'patient_stock_out')),
+                     strong(textOutput(outputId = 'ctr_stock_out')),
+                     strong(textOutput(outputId = 'nctr_stock_out')),
+                     strong(textOutput(outputId = 'total_admissions_out')),
+                     strong(textOutput(outputId = 'avg_los_out')),
+                     strong(textOutput(outputId = 'nctr_per_bed_out')),
+                     strong(textOutput(outputId = 'pre_DRD_los_out')),
+                     strong(textOutput(outputId = 'post_DRD_los_out')),
             ),
             tabPanel('Length of delay calculator',
-                     # h2("Inputs:"),
-                     # numericInput(inputId = 'nctr_per_bed_in', 
-                     #              label = 'Select NCTR per bed:', 
-                     #              value = 0.5, 
-                     #              min = 0, 
-                     #              max = 1, 
-                     #              step = 0.01),
-                     # numericInput(inputId = 'post_DRD_lod', 
-                     #              label = 'Select average post-DRD length of delay (all patients):', 
-                     #              value = 3, 
-                     #              min = 0, 
-                     #              max = 30, 
-                     #              step = 1),
-                     # numericInput(inputId = 'bed_occ_2', 
-                     #              label = 'Select bed occupancy (%):', 
-                     #              value = 90, 
-                     #              min = 50, 
-                     #              max = 100, 
-                     #              step = 1),
-                     # numericInput(inputId = 'ga_beds_2', 
-                     #              label = 'Select G&A beds:', 
-                     #              value = 100, 
-                     #              min = 50, 
-                     #              max = 2000, 
-                     #              step = 10),
-                     # h2("Ouputs:"),
-                     # strong(textOutput(outputId = 'patient_stock')),
-                     # strong(textOutput(outputId = 'ctr_stock')),
-                     # strong(textOutput(outputId = 'nctr_stock')),
-                     # strong(textOutput(outputId = 'total_admissions')),
-                     # strong(textOutput(outputId = 'avg_los')),
-                     # strong(textOutput(outputId = 'nctr_per_bed'))
                 )
             )
         )
     )
 
 server <- function(input, output, session){
+    
+    #calculator A
+    nctr_per_bed_val <- reactive({ifelse(is.na(input$nctr_per_bed_in),
+                                     nctr_stock_val() / input$ga_beds_in,
+                                     input$nctr_per_bed_in)})
+    #calculator B
+    pre_DRD_los_val <- reactive({ifelse(is.na(input$pre_DRD_los_in),
+                                        avg_los_val() - input$post_DRD_los_in,
+                                        input$pre_DRD_los_in)})
+    #calculator C
+    post_DRD_los_val <- reactive({ifelse(is.na(input$post_DRD_los_in),
+                                         nctr_stock_val() / total_admissions_val(),
+                                         input$post_DRD_los_in)})
+        
     patient_stock_val <- reactive({
-        (input$bed_occ/100) * input$ga_beds
+        (input$bed_occ_in/100) * input$ga_beds_in
     })
     
     avg_los_val <- reactive({
-        input$pre_DRD_los + input$post_DRD_los
+        input$pre_DRD_los_in + input$post_DRD_los_in
     })
     
     total_admissions_val <- reactive({
@@ -98,39 +94,43 @@ server <- function(input, output, session){
     })
     
     ctr_stock_val <- reactive({
-        total_admissions_val() * input$pre_DRD_los
+        total_admissions_val() * input$pre_DRD_los_in
     })
     
     nctr_stock_val <- reactive({
         patient_stock_val() - ctr_stock_val()
     })
     
-    nctr_per_bed <- reactive({
-        nctr_stock_val() / input$ga_beds
-    })
-    
-    output$patient_stock <- renderText({
+    output$patient_stock_out <- renderText({
         paste0("Total admitted patient stock: ", round(patient_stock_val(), 1))
     })
     
-    output$avg_los <- renderText({
+    output$avg_los_out <- renderText({
         paste0("Total average length of stay: ", round(avg_los_val(), 1))
     })
     
-    output$total_admissions <- renderText({
+    output$total_admissions_out <- renderText({
         paste0("Total admissions (Patients per day): ", round(total_admissions_val(), 1))
     })
     
-    output$ctr_stock <- renderText({
+    output$ctr_stock_out <- renderText({
         paste0("CTR stock: ", round(ctr_stock_val(), 0))
     })
     
-    output$nctr_stock <- renderText({
+    output$nctr_stock_out <- renderText({
         paste0("NCTR stock: ", round(nctr_stock_val(), 0))
     })
     
-    output$nctr_per_bed <- renderText({
-        paste0("NCTR per bed: ", round(nctr_per_bed(), 2))
+    output$nctr_per_bed_out <- renderText({
+        paste0("NCTR per bed: ", round(nctr_per_bed_val(), 2))
+    })
+    
+    output$pre_DRD_los_out <- renderText({
+        paste0("Average pre-DRD length of stay: ", round(pre_DRD_los_val(), 1))
+    })
+    
+    output$post_DRD_los_out <- renderText({
+        paste0("Average post-DRD length of stay: ", round(post_DRD_los_val(), 1))
     })
 }
 
